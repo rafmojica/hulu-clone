ebd254567a820da40ac228db3c5e299abb5cdc28
rafa <rafumojica1@gmail.com>
2022-10-13 17:49:10 +0800

    initial commit

---
+++ components/Nav.js
@@ -0,0 +1,20 @@
+import requests from "../utilities/requests";
+
+function Nav() {
+  return (
+    <nav>
+      <div className="flex">
+        {Object.entries(requests).map(([key, { title, url }]) => {
+          <h2
+            key={key}
+            className="cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
+          >
+            {title}
+          </h2>;
+        })}
+      </div>
+    </nav>
+  );
+}
+
+export default Nav;
